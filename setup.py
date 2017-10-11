92afc8984b3aa1ddb9a4835d574fa930fbfa642a
Marco Aceti <personale@marcoaceti.it>
2017-10-11 17:31:04 +0200

    Ready for PyPI

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name='classeviva',
+    packages=['classeviva'],
+    package_dir={'': 'src'},
+    version='0.1',
+    description='Classe Viva Python API',
+    author='Marco Aceti',
+    author_email='mail@marcoaceti.it',
+    url='https://github.com/MarcoBuster/ClasseVivaAPI',
+    download_url='https://github.com/MarcoBuster/ClasseVivaAPI/archive/0.1.tar.gz',
+    keywords=['classe viva', 'school', 'api', 'rest'],
+    classifiers=[],
+)
